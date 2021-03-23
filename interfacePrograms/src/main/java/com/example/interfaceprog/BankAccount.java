78894c9ebf959441a0edbf2a52649376d41b4d75
ccccljs <cho38suk@hamail.net>
2021-03-23 13:27:00 +0900

    master branch

---
+++ interfacePrograms/src/main/java/com/example/interfaceprog/BankAccount.java
@@ -0,0 +1,16 @@
+package com.example.interfaceprog;
+
+public class BankAccount {
+    private double balance; // obj.getBalance();
+    private int accountNumber; // obj.getAccountNumber();
+    private static int lastAssignedNumber=1000; // BankAccount.lastAssignedNumber
+    public BankAccount(double balance) {
+        this.balance = balance;
+        lastAssignedNumber++;
+        accountNumber=lastAssignedNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
